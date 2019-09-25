e7848f1d1348128949cea859042aeb3d21a7f1dd
fireairforce <1344492820@qq.com>
2019-09-25 22:31:03 +0800

    init project

---
+++ READMD.md
@@ -0,0 +1,19 @@
+## Notes
+使用`koa-generator`生成的脚手架
+
+安装:
+```js
+npm install -g koa-generator
+```
+
+生成:
+```js
+koa2 -e koa2-learn // 生成ejs为模板引擎的koa脚手架
+koa2 koa2-learn  // 否则就是以pug为模板引擎
+```
+
+运行起来的方法为(这玩意儿用`nodemon`在跑，所以你懂我意思):
+```js
+npm run dev
+```
+
---
+++ routes/index.js
@@ -0,0 +1,20 @@
+const router = require('koa-router')()
+
+router.get('/', async (ctx, next) => {
+  global.console.log(next);
+  await ctx.render('index', {
+    title: 'Hello Koa 2!'
+  })
+})
+
+router.get('/string', async (ctx, next) => {
+  ctx.body = 'koa2 string'
+})
+
+router.get('/json', async (ctx, next) => {
+  ctx.body = {
+    title: 'koa2 json'
+  }
+})
+
+module.exports = router
